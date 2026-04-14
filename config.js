2daa2651d2d7a8950ce521f39fcae38d81b30b45
wnzyjs <3545115050@QQ.COM>
2026-04-14 14:50:43 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  // Paste your deployed Google Apps Script web app URL here.
+  // Example: "https://script.google.com/macros/s/ABC123/exec"
+  apiBaseUrl: "https://script.google.com/macros/s/AKfycbxLXSSpxBRfF59lnO_wL33P8gCkfF6RZaTKu0IQtgWukrufmgSVkOU0NsUnFGbIQtRt/exec",
+  sharedModeLabel: "Shared Google Sheet",
+  refreshIntervalMs: 15000
+};
